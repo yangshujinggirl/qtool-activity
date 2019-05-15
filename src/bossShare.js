3d44964ddd3a483cd723650d9c59e199778c0d7c
杨静 <yj@qtoolsbaby.com>
2019-05-15 14:44:59 +0800

    修改文案,新增h5原生交互readme文档

--- src/bossShare.js
+++ src/bossShare.js
@@ -10,11 +10,10 @@ rem(100,1);
 
 const titleMap = {
   0:"520领Qtools好礼，快来看看都有啥",
-  1:"全球母婴狂欢节，邀请闺蜜一起领更多优惠",
+  1:"全球母婴狂欢节，呼唤闺蜜一起血拼",
   2:"妈妈们，这可能是今年最低价的，进口母婴好物！",
   3:"母婴节快到了，买点好的犒劳当妈的自己",
-  4:"奶粉突然大降价，Qtools全球母婴狂欢节全是优惠！",
-  5:"进口母婴囤货季，邀请闺蜜还能领好礼！",
+  4:"进口母婴囤货季，Qtools邀请你和闺蜜一起血拼！",
 }
 $(document).ready(function() {
   new Vue({
@@ -45,7 +44,7 @@ $(document).ready(function() {
         let vm  = this;
         var imageUrl = "https://qcampfile.oss-cn-shanghai.aliyuncs.com/activity_share.png";
         if(this.isClick) {
-          var index = Math.floor(Math.random()*6);
+          var index = Math.floor(Math.random()*5);
           window.Qtools.share(JSON.stringify({
             type:1,
             title:titleMap[index],
@@ -53,7 +52,7 @@ $(document).ready(function() {
             path:'pages/welcome/welcome?scene=0_'+vm.spshopid,
             imageUrl,
             isPic:1,
-            copyText:'宝宝们，Qtools全球母婴狂欢节来啦，注册即可领取520大礼包，参与邀请闺蜜的活动，还有机会兑换精美礼品，数量有限，先到先得哦！'
+            copyText:'Qtools全球母婴狂欢节来啦！'
           }))
         }
       }
